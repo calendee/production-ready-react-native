cee8fc8f0f0b6242f9be60bf75bad70c7adf52e8
Justin Noel <justin.noel@calendee.com>
2019-02-11 16:30:28 -0600

    Clearing Error Messages

--- App/config/sagas.js
+++ App/config/sagas.js
@@ -1,4 +1,5 @@
 import { takeEvery, call, put, select } from 'redux-saga/effects';
+import { delay } from 'redux-saga';
 
 import {
   CHANGE_BASE_CURRENCY,
@@ -45,10 +46,25 @@ const fetchLatestConversionRates = function* ({ currency }) {
   }
 };
 
+// If there was a network error, it gets persisted to localstorage
+// When the app reloads, the error is still there and gets displayed
+// Since the error is part of the conversions state, we can't blacklist it
+// To overcome this, we let the error display initially for 4 seconds
+// Then, we clear it out so it is persisted as null
+const clearConversionError = function* () {
+  const DELAY_SECONDS = 4;
+  const error = yield select(state => state.currencies.error);
+  if (error) {
+    yield delay(DELAY_SECONDS * 1000);
+    yield put({ type: CONVERSION_ERROR, error: null });
+  }
+};
+
 const rootSaga = function* () {
   yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
   yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
   yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
+  yield takeEvery(CONVERSION_ERROR, clearConversionError);
 };
 
 export default rootSaga;
