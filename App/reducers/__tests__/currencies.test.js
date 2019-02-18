e68d7b8c7a8746bcfb2ef8afed6e855e6aa40032
Justin Noel <justin.noel@calendee.com>
2019-02-18 08:14:43 -0600

    Testing Redux Reducer with Jest Snapshots

--- App/reducers/__tests__/currencies.test.js
+++ App/reducers/__tests__/currencies.test.js
@@ -1,32 +1,35 @@
 import reducer from '../currencies';
 import { getInitialConversion } from '../../actions/currencies';
 
-const initialState = {
-  baseCurrency: 'USD',
-  quoteCurrency: 'GBP',
-  amount: 100,
-  conversions: {},
-  error: null,
-};
+// NOTE: Replaced the original TDD style with snapshots
+// const initialState = {
+//   baseCurrency: 'USD',
+//   quoteCurrency: 'GBP',
+//   amount: 100,
+//   conversions: {},
+//   error: null,
+// };
 
 it('sets initial state', () => {
-  const expected = { ...initialState };
-  const actual = reducer(undefined, {});
-  expect(actual).toEqual(expected);
+  // const expected = { ...initialState };
+  // const actual = reducer(undefined, {});
+  // expect(actual).toEqual(expected);
+  expect(reducer(undefined, {})).toMatchSnapshot();
 });
 
 it('sets nested data on initial fetch', () => {
-  const expected = {
-    ...initialState,
-    conversions: {
-      USD: {
-        date: '',
-        isFetching: true,
-        rates: {},
-      },
-    },
-  };
+  // const expected = {
+  //   ...initialState,
+  //   conversions: {
+  //     USD: {
+  //       date: '',
+  //       isFetching: true,
+  //       rates: {},
+  //     },
+  //   },
+  // };
 
-  const actual = reducer(undefined, getInitialConversion());
-  expect(actual).toEqual(expected);
+  // const actual = reducer(undefined, getInitialConversion());
+  // expect(actual).toEqual(expected);
+  expect(reducer(undefined, getInitialConversion())).toMatchSnapshot();
 });
