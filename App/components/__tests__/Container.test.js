6ad11032589e4c989db9152efbf4e4315df43d14
Justin Noel <justin.noel@calendee.com>
2019-02-18 07:27:03 -0600

    Component Snapshot Testing with Jest

--- App/components/__tests__/Container.test.js
+++ App/components/__tests__/Container.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View } from 'react-native';
 import renderer from 'react-test-renderer';
 
 import { Container, styles } from '../Container';
@@ -9,6 +10,20 @@ it('exports a styles object', () => {
 
 it('renders successfully without children', () => {
   const rendered = renderer.create(<Container />).toJSON();
-  console.log(rendered);
   expect(rendered).toBeTruthy();
+});
+
+it('renders children props', () => {
+  const rendered = renderer
+    .create(<Container>
+      <View />
+            </Container>)
+    .toJSON();
+
+  expect(rendered).toMatchSnapshot();
+});
+
+it('uses the specified backgroundColor if provided', () => {
+  const rendered = renderer.create(<Container backgroundColor="red" />).toJSON();
+  expect(rendered).toMatchSnapshot();
 });
