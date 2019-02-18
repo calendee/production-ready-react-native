9a060f3822a74cc5609e8d04146e9bb54e8104bd
Justin Noel <justin.noel@calendee.com>
2019-02-18 07:56:03 -0600

    Testing Redux Actions with Jest Snapshots

--- App/actions/__tests__/currencies.test.js
+++ App/actions/__tests__/currencies.test.js
@@ -2,30 +2,33 @@ import { changeCurrencyAmount, getInitialConversion } from '../currencies';
 
 describe('getInitialConversion', () => {
   it('creates a properly formatted action', () => {
-    const expected = { type: 'GET_INITIAL_CONVERSION' };
-    const actual = getInitialConversion();
-    expect(actual).toEqual(expected);
+    // const expected = { type: 'GET_INITIAL_CONVERSION' };
+    // const actual = getInitialConversion();
+    // expect(actual).toEqual(expected);
+    expect(getInitialConversion()).toMatchSnapshot();
   });
 });
 
 describe('changeCurrencyAmount', () => {
   it('creates an action with an amount of 100', () => {
-    const expected = {
-      type: 'CHANGE_CURRENCY_AMOUNT',
-      amount: 100,
-    };
+    // const expected = {
+    //   type: 'CHANGE_CURRENCY_AMOUNT',
+    //   amount: 100,
+    // };
 
-    const actual = changeCurrencyAmount(100);
-    expect(actual).toEqual(expected);
+    // const actual = changeCurrencyAmount(100);
+    // expect(actual).toEqual(expected);
+    expect(changeCurrencyAmount(100)).toMatchSnapshot();
   });
 
   it('converts a string value to a float', () => {
-    const expected = {
-      type: 'CHANGE_CURRENCY_AMOUNT',
-      amount: 100,
-    };
+    // const expected = {
+    //   type: 'CHANGE_CURRENCY_AMOUNT',
+    //   amount: 100,
+    // };
 
-    const actual = changeCurrencyAmount('100');
-    expect(actual).toEqual(expected);
+    // const actual = changeCurrencyAmount('100');
+    // expect(actual).toEqual(expected);
+    expect(changeCurrencyAmount('100')).toMatchSnapshot();
   });
 });
