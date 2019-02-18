7c698cb37a2d61e4cc2e4c29cc2ce2fe2dfd3178
Justin Noel <justin.noel@calendee.com>
2019-02-18 07:11:17 -0600

    Organizing Tests and Ensuring Components Will Render

---
+++ App/components/__tests__/Container.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Container, styles } from '../Container';
+
+it('exports a styles object', () => {
+  expect(typeof styles).toBe('object');
+});
+
+it('renders successfully without children', () => {
+  const rendered = renderer.create(<Container />).toJSON();
+  console.log(rendered);
+  expect(rendered).toBeTruthy();
+});
