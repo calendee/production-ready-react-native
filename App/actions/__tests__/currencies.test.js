737ee33434cc7671e579a830142fda30d6fd3131
Justin Noel <justin.noel@calendee.com>
2019-02-18 07:42:04 -0600

    Testing Redux Actions

---
+++ App/actions/__tests__/currencies.test.js
@@ -0,0 +1,31 @@
+import { changeCurrencyAmount, getInitialConversion } from '../currencies';
+
+describe('getInitialConversion', () => {
+  it('creates a properly formatted action', () => {
+    const expected = { type: 'GET_INITIAL_CONVERSION' };
+    const actual = getInitialConversion();
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('changeCurrencyAmount', () => {
+  it('creates an action with an amount of 100', () => {
+    const expected = {
+      type: 'CHANGE_CURRENCY_AMOUNT',
+      amount: 100,
+    };
+
+    const actual = changeCurrencyAmount(100);
+    expect(actual).toEqual(expected);
+  });
+
+  it('converts a string value to a float', () => {
+    const expected = {
+      type: 'CHANGE_CURRENCY_AMOUNT',
+      amount: 100,
+    };
+
+    const actual = changeCurrencyAmount('100');
+    expect(actual).toEqual(expected);
+  });
+});
